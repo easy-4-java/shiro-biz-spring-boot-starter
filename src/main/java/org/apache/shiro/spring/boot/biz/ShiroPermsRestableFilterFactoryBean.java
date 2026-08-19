86c5a5a67ef628d81f1c62df10b0fcafe8937d4a
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-19 21:23:47 +0800

    fix: resolve compilation errors for Maven 4.0.0-rc-5
    Remove broken nested block comments in ShiroPermsRestableFilterFactoryBean
    and RsaKeyPairHttpServlet that caused 'illegal start of type' errors.
    
    @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/biz/ShiroPermsRestableFilterFactoryBean.java
+++ src/main/java/org/apache/shiro/spring/boot/biz/ShiroPermsRestableFilterFactoryBean.java
@@ -27,61 +27,7 @@ import java.util.Map;
  * @see <a href="https://www.cnblogs.com/007sx/p/7381475.html">Dynamic Permission Reference</a>
  */
 public class ShiroPermsRestableFilterFactoryBean extends ShiroBizFilterFactoryBean  {
-	
-	/*@Autowired
-    private RolesPermissionsService rolesPermissionsService;
-	
-	 *//**
-     * 初始化设置过滤链
-     *//*
-    @Override
-    /**
-     * <p>Sets the filter chain definitions.</p>
-     * @param definitions
-     */
-    public void setFilterChainDefinitions(String definitions) {
-//        String token =  manageUserService.getAdminToken(0);
 
-        //可从数据库读取后，添加至过滤链，参考此处已注释的代码
-        definition = definitions;//记录配置的静态过滤链
-//        List<Permission> permissions = permissService.findAll();
-
-        List<RolesPermissions> rolesPermissions = rolesPermissionsService.selectList(new EntityWrapper<>());
-        Set<String> urls = new LinkedHashSet<>();
-        for (RolesPermissions rolesPermission : rolesPermissions) {
-            urls.add(rolesPermission.getUrl());
-        }
-
-        Map<String,String> otherChains = new HashMap<>();
-        for (String url : urls) {
-            StringBuilder roleOrFilters = new StringBuilder();
-            for (int i = 0; i < rolesPermissions.size(); i++) {
-                if (Objects.equals(url, rolesPermissions.get(i).getUrl())) {
-                    if (i == 0) {
-                        roleOrFilters.append(rolesPermissions.get(i).getRolesName());
-                    }else{
-                        roleOrFilters.append(",").append(rolesPermissions.get(i).getRolesName());
-                    }
-                }
-            }
-            String rolesStr = roleOrFilters.toString();
-            if (!"".equals(rolesStr)) {
-                otherChains.put(url, "roleOrFilter["+rolesStr+"]"); //  /discover/newstag  authc,roles[user,admin]
-            }
-        }
-        //加载配置默认的过滤链
-        Ini ini = new Ini();
-        ini.load(definitions);
-        Ini.Section section = ini.getSection(IniFilterChainResolverFactory.URLS);
-        if (CollectionUtils.isEmpty(section)) {
-            section = ini.getSection(Ini.DEFAULT_SECTION_NAME);
-        }
-        //加上数据库中过滤链
-        section.putAll(otherChains);
-        section.put("/**", "anon");
-        setFilterChainDefinitionMap(section);
-    }*/
-	
 	/**
 	 * Sets the filter chain definition map, allowing dynamic permission definitions to be merged
 	 * with the static filter chain configuration.
